ca72100fa5fdcc288413f847847beb73c59f93f1
Sebastian Aigner <sebastian.aigner@tum.de>
2021-12-29 11:21:44 +0100

    Aldous-Broder

---
+++ src/main/kotlin/AldousBroder.kt
@@ -0,0 +1,31 @@
+import java.io.File
+import javax.imageio.ImageIO
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+class AldousBroder {
+    fun on(grid: Grid) {
+        var cell = grid.randomCell()
+        var unvisited = grid.size - 1
+        while (unvisited > 0) {
+            val neighbor = cell.neighbors().random()
+            if (neighbor.links().isEmpty()) {
+                cell.link(neighbor)
+                unvisited--
+            }
+            cell = neighbor
+        }
+    }
+}
+
+@OptIn(ExperimentalTime::class)
+fun main() {
+    repeat(6) {
+        val grid = ColoredGrid(200, 200)
+        println(measureTime { AldousBroder().on(grid) })
+        val middle = grid[grid.rows / 2, grid.columns / 2]!!
+        grid.distances = middle.distances()
+        ImageIO.write(grid.toImage(), "png", File("AldousBroder$it.png"))
+    }
+
+}
